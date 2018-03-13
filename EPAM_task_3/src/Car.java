edd636958f6fa0a44369270c9bf56bd71acbc0d6
kstaroverov <kstaroverov@mrkt-kirill.mylands.local>
2018-03-13 16:57:12 +0200

    Создан класс CarStatistics

---
+++ EPAM_task_3/src/Car.java
@@ -0,0 +1,34 @@
+
+public class Car {
+	private int id;
+	private String manufacturer;
+	private String model;
+	private int year;
+	private String color;
+	private double price;
+	private String registrationnumber;
+	
+	public int getId() {
+		return id;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public String getModel() {
+		return model;
+	}
+	public int getYear() {
+		return year;
+	}
+	public String getColor() {
+		return color;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public String getRegistrationnumber() {
+		return registrationnumber;
+	}
+	
+	
+}
--- EPAM_task_3/src/CarStatistics.java
+++ EPAM_task_3/src/CarStatistics.java
@@ -8,7 +8,6 @@ public class CarStatistics<T extends Car> {
 	}
 
 	public LinkedList<T> selectByManufacturer(String manufacturer, LinkedList<T> arr) {
-		
 		LinkedList<T> arrByManufacturer = new LinkedList<T>();
 
 		for (T x : arr) {
@@ -18,23 +17,30 @@ public class CarStatistics<T extends Car> {
 		}
 		return arrByManufacturer;
 	}
-	
+
 	public LinkedList<T> selectByModelAndYears(String model, int years, LinkedList<T> arr) {
-		
 		LinkedList<T> arrByModelandYears = new LinkedList<T>();
-		
+
 		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-		
+
 		for (T x : arr) {
-			if ((x.getManufacturer().equals(model) && (currentYear - years) < x.getYear())) {
+			if ((x.getModel().equals(model) && (currentYear - years) < x.getYear())) {
 				arrByModelandYears.add(x);
 			}
 		}
-		return arrByModelandYears;	
+		return arrByModelandYears;
 	}
-	
-	public static void main(String[] args) {
-		System.out.println(Calendar.getInstance().get(Calendar.YEAR));
+
+	public LinkedList<T> selectByYearAndPrice(int year, int price, LinkedList<T> arr) {
+		LinkedList<T> ByYearAndPrice = new LinkedList<T>();
+
+		for (T x : arr) {
+			if (x.getYear() == year && x.getPrice() > price) {
+				ByYearAndPrice.add(x);
+			}
+		}
+
+		return ByYearAndPrice;
 	}
 
 }
